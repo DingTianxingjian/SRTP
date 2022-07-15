0a5b4291fa07b9e2057987295d8fd5a07e1ef09d
Pei Yuhang <100062345+Pikachu-Yuhang@users.noreply.github.com>
2022-07-15 17:02:17 +0800

    Update npz_to_obj.py
    Modify the program into functions

--- utility/npz_to_obj.py
+++ utility/npz_to_obj.py
@@ -1,11 +1,22 @@
 import numpy as np
 
-# read point information from .npz file
-data = np.load('pointcloud.npz')
-points = data['points']
+'''
+  Transfer points array to .obj file
+  @param obj_file_name: The given name of .obj file
+  @param points: The array of points, where each point is 3-dimensional
+'''
+def PointsToObj(obj_file_name, points):
+    with open(file = obj_file_name, mode = 'w') as obj:
+        for i in range(len(points)):
+            output = 'v ' + repr(points[i][0]) + ' ' + repr(points[i][1]) + ' ' + repr(points[i][2]) + '\n'
+            obj.write(output)
 
-# # create an .obj file and write point data into it
-with open(file = 'pointcloud.obj', mode = 'w') as obj:
-    for i in range(len(points)):
-        output = 'v ' + repr(points[i][0]) + ' ' + repr(points[i][1]) + ' ' + repr(points[i][2]) + '\n'
-        obj.write(output)
+'''
+  Transfer .npz file to .obj file
+  @param obj_file_name: The given name of .obj file
+  @param npz_file_name: .npz file extracted from ShapeNet, containing 3-dimensional coordinates of all the points
+'''
+def NpzToObj(obj_file_name, npz_file_name):
+    data = np.load(npz_file_name)
+    points = data['points']
+    PointsToObj(obj_file_name, points)
